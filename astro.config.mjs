509473b437ec016fd90a2c6643076afc95a759fe
casungo <alessandro@casungo.top>
2025-05-20 22:21:07 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://casungo.github.io",
-  base: "testSlashAstro",
+  base: "/testSlashAstro",
   integrations: [mdx(), sitemap()],
 });
